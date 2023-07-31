2c61c8c051d342ffcbd2a6af0ead13d50fe118c0
Luana Oliveiram <luanasilva.jua@gmail.com>
2023-07-31 18:32:39 -0300

    Ajustes no estilo da main

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -1,13 +1,18 @@
-import { Inter } from 'next/font/google'
+import { Lato } from 'next/font/google'
 import Image from "next/image"
 import pessoas from "../../assets/imagens/pessoas.svg"
 
+const lato = Lato ({
+    subsets: ['latin'],
+    weight: '400',
+});
+
 export default function Main() {
     return (
-        <main className="flex min-h-screen flex-col">
+        <main className={`${lato.className} flex min-h-screen flex-col`}>
             <div className='m-5'>
                 <div>
-                    <h1 className="text-center text-xl p-3 font">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+                    <h1 className="text-center text-xl p-3" >Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
                     <Image  src={pessoas} alt='Imagem da logo'/>
                 </div>
                 <div className='py-5'>
@@ -18,17 +23,17 @@ export default function Main() {
                 </div>
                 <div>
                     <ul className='flex flex-col justify-center items-center my-4'>
-                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4'>Missão</li>
-                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4'>Visão</li>
-                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4'>Valores</li>
+                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4 p-2'>Missão</li>
+                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4 p-2'>Visão</li>
+                        <li className='w-60 h-40 bg-gray-200 rounded-3xl text-center my-4 p-2'>Valores</li>
                     </ul>
                 </div>
 
             </div>
-            <div className='bg-red-600 text-center h-60'>
+            <div className='bg-red-600 text-center h-60 p-3'>
                 <p className='text-white '>Apoiadores</p>
             </div>
-            <div className='h-60 text-center'>
+            <div className='h-60 text-center p-3'>
                 <p>Contato</p>
             </div>
         </main>
